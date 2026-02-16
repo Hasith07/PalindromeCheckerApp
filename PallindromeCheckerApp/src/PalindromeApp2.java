c48c0c730f04724c7a375d57ee5bf03a1f2856d5
Hasith07 <saihasith22@gmail.com>
2026-02-16 15:41:21 +0530

    UC1 Application Entry & Welcome Message

---
+++ PallindromeCheckerApp/src/PalindromeApp2.java
@@ -0,0 +1,6 @@
+public class PalindromeApp2 {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker App\nVersion 1.0\nSystem Intialized..");
+    }
+
+}
